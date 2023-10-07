14f49457f83ec6370d01c4b1aedcd177a6980ebb
drealdumore <sireroyce1@gmail.com>
2023-10-07 18:15:19 +0100

    Added the feature so user can rename playlists

--- src/app/userpage/history.component.ts
+++ src/app/userpage/history.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { AppService } from '../shared/app.service';
 
 @Component({
-  templateUrl: './History.component.html',
+  templateUrl: './history.component.html',
 })
 export class HistoryComponent {
   title: string = 'History';
